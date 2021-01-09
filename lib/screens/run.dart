c20dd0cfc7eb568e68c67b315be26e19b760e335
vish198910 <vish19810@gmail.com>
2021-01-09 21:02:56 +0530

    map added

--- lib/screens/run.dart
+++ lib/screens/run.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Run extends StatefulWidget {
   @override
@@ -9,6 +10,142 @@ class Run extends StatefulWidget {
 class _RunState extends State<Run> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: Text(
+          "Run",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: MapView(),
+    );
+  }
+}
+
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  // Initial location of the Map view
+  CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0));
+
+// For controlling the view of the Map
+  GoogleMapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    // Determining the screen width & height
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    return Container(
+      height: height,
+      width: width,
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            // TODO: Add Map View
+            GoogleMap(
+              initialCameraPosition: _initialLocation,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              mapType: MapType.normal,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: false,
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ClipOval(
+                      child: Material(
+                        color: Colors.blue[100], // button color
+                        child: InkWell(
+                          splashColor: Colors.blue, // inkwell color
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Icon(Icons.add),
+                          ),
+                          onTap: () {
+                            mapController.animateCamera(
+                              CameraUpdate.zoomIn(),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ClipOval(
+                      child: Material(
+                        color: Colors.blue[100], // button color
+                        child: InkWell(
+                          splashColor: Colors.blue, // inkwell color
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Icon(Icons.remove),
+                          ),
+                          onTap: () {
+                            mapController.animateCamera(
+                              CameraUpdate.zoomOut(),
+                            );
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                Container(
+                  child: Center(
+                    child: Text(
+                      "Start",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                  width: 150,
+                  height: 150,
+                  decoration: new BoxDecoration(
+                    gradient: RadialGradient(colors: [
+                      Colors.black,
+                      Colors.black,
+                      Colors.black,
+                      Colors.black,
+                      Colors.black,
+                      Colors.black,
+                      Colors.black,
+                      Colors.white.withOpacity(0.1),
+                      Colors.white.withOpacity(0.4),
+                      Colors.white.withOpacity(0.3),
+                      Colors.white.withOpacity(0.2),
+                      Colors.black,
+                      Colors.white,
+                    ]),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Center(
+                  child: Container(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

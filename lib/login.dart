e24c346e5bf6fb0db32f3b8ac6476f1cf00de826
keshav suman <keshavsuman96@gmail.com>
2021-02-01 05:46:12 +0530

    Commit from keshav suman

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebolt/screens/user_settings.dart';
 import 'package:firebolt/sign_up.dart';
 import 'package:firebolt/style/color.dart';
 import 'package:flutter/material.dart';
@@ -16,10 +17,11 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController passwordController = new TextEditingController();
 
   bool passwordVisible = false;
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text(
@@ -33,135 +35,166 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  padding: EdgeInsets.all(6),
-                  child: ListTile(
-                    title: TextFormField(
-                      cursorColor: boltPrimaryColor,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          hintText: "Enter your Email",
-                          labelText: "Email",
-                          labelStyle: TextStyle(color: boltPrimaryColor)),
-                      controller: emailController,
+          child: Form(
+            key: _formkey,
+                      child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black),
                     ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  padding: EdgeInsets.all(6),
-                  child: ListTile(
-                    title: TextFormField(
-                      controller: passwordController,
-                      cursorColor: boltPrimaryColor,
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: passwordVisible ? false : true,
-                      decoration: InputDecoration(
-                        hintText: "Enter your Password",
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          color: boltPrimaryColor,
-                        ),
-                      ),
-                    ),
-                    trailing: GestureDetector(
-                      child: Icon(passwordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onTap: () {
-                        setState(() {
-                          if (passwordVisible) {
-                            passwordVisible = false;
-                          } else {
-                            passwordVisible = true;
+                    padding: EdgeInsets.all(6),
+                    child: ListTile(
+                      title: TextFormField(
+                        cursorColor: boltPrimaryColor,
+                        validator: (value){
+                          if(value.isEmpty)
+                          {
+                            return "Please Enter an email";
                           }
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              ButtonTheme(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    onPressed: () async {
-                      try {
-                        UserCredential userCredential = await FirebaseAuth
-                            .instance
-                            .signInWithEmailAndPassword(
-                                email: emailController.text,
-                                password: passwordController.text);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
-                          print('No user found for that email.');
-                        } else if (e.code == 'wrong-password') {
-                          print('Wrong password provided for that user.');
-                        }
-                      }
-                    },
-                    elevation: 5.0,
-                    color: Colors.black,
-                    textColor: Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
-                      child: Text(
-                        'Login',
-                        style: TextStyle(fontSize: 18),
+                          if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value))
+                          {
+                            print('yes');
+                            return "Please Enter a valid email";
+                          }
+                          else{
+                            return null;
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            hintText: "Enter your Email",
+                            labelText: "Email",
+                            labelStyle: TextStyle(color: boltPrimaryColor)),
+                        controller: emailController,
                       ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                width: double.infinity,
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Are you a new User?  ",
-                        style: TextStyle(fontSize: 15),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
                       ),
-                      GestureDetector(
-                        child: Text(
-                          "Signup Here",
-                          style: TextStyle(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.all(6),
+                    child: ListTile(
+                      title: TextFormField(
+                        controller: passwordController,
+                        cursorColor: boltPrimaryColor,
+                        validator: (value){
+                          if(value.isEmpty)
+                          {
+                            return "Please enter a password";
+                          }
+                          if(value.length<6)
+                          {
+                            return "Password length must be 6 or more";
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: passwordVisible ? false : true,
+                        decoration: InputDecoration(
+                          hintText: "Enter your Password",
+                          labelText: "Password",
+                          labelStyle: TextStyle(
                             color: boltPrimaryColor,
-                            fontSize: 15,
                           ),
                         ),
+                      ),
+                      trailing: GestureDetector(
+                        child: Icon(passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off),
                         onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return SignUpPage();
-                          }));
+                          setState(() {
+                            if (passwordVisible) {
+                              passwordVisible = false;
+                            } else {
+                              passwordVisible = true;
+                            }
+                          });
                         },
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                ButtonTheme(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RaisedButton(
+                      onPressed: () async {
+                        if(_formkey.currentState.validate())
+                        {try {
+                          UserCredential userCredential = await FirebaseAuth
+                              .instance
+                              .signInWithEmailAndPassword(
+                                  email: emailController.text,
+                                  password: passwordController.text);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            print('No user found for that email.');
+                          } else if (e.code == 'wrong-password') {
+                            print('Wrong password provided for that user.');
+                          }
+                        }
+                        }
+                      },
+                      elevation: 5.0,
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Are you a new User?  ",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        GestureDetector(
+                          child: Text(
+                            "Signup Here",
+                            style: TextStyle(
+                              color: boltPrimaryColor,
+                              fontSize: 15,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              // return SignUpPage();
+                              return UserSettings();
+                            }));
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),

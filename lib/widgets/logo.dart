e892e1ff48bba4986e6f3e5099a9d34ddacc3509
vish198910 <vish19810@gmail.com>
2021-02-07 09:27:53 +0530

    logo changes

--- lib/widgets/logo.dart
+++ lib/widgets/logo.dart
@@ -4,33 +4,8 @@ class LOGO extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ListTile(
-            title: Center(
-              child: Text(
-                "BOLT",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 80
-                ),
-              ),
-            ),
-          ),
-          ListTile(
-            title: Center(
-              child: Text(
-                "Watches",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30
-                ),
-              ),
-            ),
-          )
-        ],
+      child: Image.asset(
+        "images/logo.jpeg",
       ),
     );
   }
